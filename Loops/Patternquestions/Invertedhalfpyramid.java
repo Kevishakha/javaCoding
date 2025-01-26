35a1cd5ad9720e6ee5b690fb4e6720eb089cb1af
vishakha <kevishakha@gmail.com>
2025-01-26 15:47:22 +0530

    Create Invertedhalfpyramid.java
    pattern inverted halfpyramid
    *****
    ****
    ***
    **
    *

---
+++ Loops/Patternquestions/Invertedhalfpyramid.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+      for(int j=4;j>=i;j--){
+          System.out.print("*");
+      }
+      System.out.println();
+      }
+      
+    }
+    
+  }
