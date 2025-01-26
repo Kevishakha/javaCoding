9349a290ce46061959958bfa6698bf5d655fdd66
vishakha <kevishakha@gmail.com>
2025-01-26 16:07:23 +0530

    Create Invertedhalfpyramid180deg.java
    Here the pattern is inverted halfpyramid in 180degree rotating
        *
       **
      ***
     ****
    *****

---
+++ Loops/Patternquestions/Invertedhalfpyramid180de.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+      int n=5;
+    for (int i = 1; i <= n; i++) {
+              // to print spaces
+      for(int j=1;j<=n-i;j++){
+          System.out.print(" ");
+      }
+      // to print stars
+      for(int j=1;j<=i;j++){
+          System.out.print("*");
+      }
+      
+
+      System.out.println();
+      }
+      
+    }
+    
+  }
