835b714853d7c9a66915e69be6fae1d5885fc12c
vishakha <kevishakha@gmail.com>
2025-01-26 15:34:49 +0530

    Add files via upload
    Solid rectangle pattern

---
+++ Loops/Patternquestions/Solidrect.java
@@ -0,0 +1,12 @@
+
+class solidrect{
+    public static void main(String[] args) {
+        System.err.println("Solid rectangle pattern");
+        for(int i=1;i<5;i++){
+            for(int j=1;j<6;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
