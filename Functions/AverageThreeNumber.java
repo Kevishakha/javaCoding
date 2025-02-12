8c27ff166b92f4d597ab189ae0ffd746e91818f9
vishakha <kevishakha@gmail.com>
2025-02-12 11:50:34 +0530

    Create AverageThreeNumber.java
     AverageThreeNumber
    
    Example input Three numbers
    returns (num1+num2+num3)/3

---
+++ Functions/AverageThreeNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void average(int num1, int num2, int num3){
+      int avg=(num1+num2+num3)/3;
+      System.out.println(avg);
+      return;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num1 =sc.nextInt(); 
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        sc.close();
+        average(num1,num2,num3);  
+    }
+}
